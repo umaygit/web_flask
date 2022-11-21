950279a711c7958416def2a1d994ac00add71085
umaygit <mahralkusumahadi@gmail.com>
2022-11-21 17:31:08 +0800

    first

---
+++ run.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__) 
+
+@app.route('/') 
+def index():
+    return render_template('index.html') 
+
+@app.route('/tambahdata') 
+def tambahdata():
+    return render_template('tambahdata.html') 
+
+@app.route('/list') 
+def listdata():
+    return render_template('listdata.html') 
+
+@app.route('/login') 
+def login():
+    return render_template('login.html') 
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
